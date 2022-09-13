d0fd08476005d531287d648f642f080bb13f901f
scardes93 <scardes93@gmail.com>
2022-09-13 14:31:53 +0600

    Add project files.

---
+++ EpamOODPAuto/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+/// <summary>
+/// Console program for Object Oriented Design Principles. 
+/// Use Singleton and Command
+/// </summary>
+namespace EpamOODPAuto
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            bool endApp = false;
+
+            // Repeat program, until user input "exit" to exit.
+            while (!endApp)
+            {
+
+
+
+
+                // Wait for the user to respond before closing.
+                Console.Write("Enter 'exit' to close the app, or press any key and Enter to continue: ");
+
+                if (Console.ReadLine() == "exit")
+                {
+                    endApp = true;
+                }
+
+                Console.WriteLine("\n"); // Friendly linespacing
+            }
+        }
+    }
+}
