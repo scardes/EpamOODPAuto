52995f04e0dac7ec0811c2a664438d89af6a8c9c
scardes93 <scardes93@gmail.com>
2022-09-27 11:47:37 +0600

    Just array yet

--- EpamOODPAuto/Program.cs
+++ EpamOODPAuto/Program.cs
@@ -15,10 +15,25 @@ namespace EpamOODPAuto
             // Repeat program, until user input "exit" to exit.
             while (!endApp)
             {
+                string? brand = string.Empty;
+                string? model = string.Empty;
+                int count;
+                int price;
+
+                Console.WriteLine("Please enter information about cars: brand, model, count, price");
+                string[] arr = Console.ReadLine().Split();
+
+                brand = arr[0];
+                model = arr[1];
+                int.TryParse(arr[2], out count);
+                int.TryParse(arr[3], out price);
+
+                Console.WriteLine($" brand= {brand},  model= {model} count= {count}, price= {price}");
 
                 AutoSingleBase asb1 = AutoSingleBase.GetAutoBase();
                 AutoSingleBase asb2 = AutoSingleBase.GetAutoBase();
 
+                //TODO This just for check (need be deleted)
                 if (asb1 == asb2)
                 {
                     Console.WriteLine("Singleton works\n");
@@ -27,6 +42,8 @@ namespace EpamOODPAuto
                 {
                     Console.WriteLine("Singleton failed");
                 }
+
+                asb1.businessLogic("Send Command");
 
 
                 // Wait for the user to respond before closing.
